040d7dfb25909630a54ac99736c35a1278a5e39b
Trinh Le <T.Le@hpa.org.nz>
2020-02-17 09:50:12 +1300

    Updates

---
+++ precache-manifest.4ff369664cfd0f87a4d3a7d7ff52ad20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24a380409ed3b6376cc69755d9905d1",
+    "url": "/lvdatri1/zema/index.html"
+  },
+  {
+    "revision": "40cc45e7b8d286063fe2",
+    "url": "/lvdatri1/zema/static/css/main.7cecca9d.chunk.css"
+  },
+  {
+    "revision": "90f0b0201f84b1725ffb",
+    "url": "/lvdatri1/zema/static/js/2.91a31370.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/lvdatri1/zema/static/js/2.91a31370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40cc45e7b8d286063fe2",
+    "url": "/lvdatri1/zema/static/js/main.a03b0848.chunk.js"
+  },
+  {
+    "revision": "86f3daf52ba5ee1726c8",
+    "url": "/lvdatri1/zema/static/js/runtime-main.20138128.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lvdatri1/zema/static/media/logo.5d5d9eef.svg"
+  }
+]);

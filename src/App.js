b1af17deadd25e219f052990b315a42a3518cda8
Trinh Le <T.Le@hpa.org.nz>
2020-02-17 10:21:57 +1300

    updated app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import TypeForm from "./components/TypeForm";
-
+import GoogleForm from "./components/GoogleForm";
 function App() {
   return (
     <div className="App">
-      <TypeForm />
+      <GoogleForm />
     </div>
   );
 }
---
+++ src/components/GoogleForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export default class GoogleForm extends Component {
+  render() {
+    return (
+      <div>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSf2Ar6QObYCl0mea8beh-M0rpMSM-GsP1ReTJG1w2WPIjeQiQ/viewform?embedded=true"
+          width="100%"
+          height="100%"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Loading…
+        </iframe>
+      </div>
+    );
+  }
+}

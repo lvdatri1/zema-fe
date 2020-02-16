3513f1c8904090809ff478885878d21899691bff
Trinh Le <T.Le@hpa.org.nz>
2020-02-17 09:45:08 +1300

    first app

---
+++ src/components/TypeForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class Typeform extends Component {
+  render() {
+    return (
+      <div>
+        <iframe
+          id="typeform-full"
+          width="100%"
+          height="100%"
+          frameborder="0"
+          src="https://tess318926.typeform.com/to/B7Ezgt"
+        ></iframe>
+        <script type="text/javascript" src="https://embed.typeform.com/embed.js"></script>
+      </div>
+    );
+  }
+}

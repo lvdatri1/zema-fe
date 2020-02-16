cc9613d08bc33a7f87c6237c652c8797f3727831
Trinh Le <T.Le@hpa.org.nz>
2020-02-17 09:55:42 +1300

    update package

--- src/components/TypeForm.js
+++ src/components/TypeForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class Typeform extends Component {
+export default class TypeForm extends Component {
   render() {
     return (
       <div>

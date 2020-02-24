fee94c2b76c491e8b4f368726c1b4e18a4a87164
Trinh Le <T.Le@hpa.org.nz>
2020-02-24 13:12:36 +1300

    Updates

---
+++ precache-manifest.41345d3130baa43640a9fef6e785f521.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8c76b9e9c99b17346b5bba07b1df6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1ec7f946524f0751664",
+    "url": "/static/css/main.fcaad973.chunk.css"
+  },
+  {
+    "revision": "e6a31e8a89a7ef8979b4",
+    "url": "/static/js/2.3d3b9210.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.3d3b9210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1ec7f946524f0751664",
+    "url": "/static/js/main.c9991f22.chunk.js"
+  },
+  {
+    "revision": "a8c5a117d0f35ffdce93",
+    "url": "/static/js/runtime-main.aaa36891.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zema-fe/precache-manifest.de419bff1fcb567d5eb04a937d100db4.js"
+  "/precache-manifest.41345d3130baa43640a9fef6e785f521.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zema-fe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
